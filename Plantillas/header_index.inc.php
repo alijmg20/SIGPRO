ba4c8e3dda69ad06a612bbf10042c5a4ddfed007
AngeYP <66929667+AngeYP@users.noreply.github.com>
2020-07-28 21:21:58 -0400

    Update header_index.inc.php

--- Plantillas/header_index.inc.php
+++ Plantillas/header_index.inc.php
@@ -6,7 +6,7 @@
 	<meta charset="utf-8">
 	<title>SIGPRO</title>
 	<!--Titulo de pestaña-->
-	<link rel="shortcut icon" type="image/x-icon" href="Vistas/Logo/2.png" />
+	<link rel="shortcut icon" type="image/x-icon" href="Vistas/logo/icono.png" />
 	<meta name="description" content="Gestion de proyectos">
 	<meta name="viewport" content="width=divice-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
 	<!--<link rel="stylesheet" href="css/bootstrap.min.css">-->

8f0f1e4fc59f7d1686ba56e0414bf1a23da09d92
Ali Mata <alijmata628@gmail.com>
2020-08-13 16:20:01 -0400

    prueba de correo

--- Modelo/enviarMensajeCliente.inc.php
+++ Modelo/enviarMensajeCliente.inc.php
@@ -1,23 +1,23 @@
 <?php
     include_once '../Controlador/conexion.inc.php';
 
-    if(isset($_POST['boton_enviar_mensaje_cliente'])){
+    if(isset($_POST['boton_enviar_mensaje_cliente']) && isset($_POST['asuntomsj']) && isset($_POST['msjalcliente']) ){
         $asunto = $_POST['asuntomsj'];
-        $mensaje_correo = $_POST['msjalcliente'];
+        $mensaje = $_POST['msjalcliente'];
 
         $sql = 'SELECT * FROM proyecto 
         INNER JOIN cliente 
         ON cliente.id=proyecto.id_cliente WHERE proyecto.id ='.$id;
         $consulta = $conexion->prepare($sql);
-        $resultados = $consulta->execute();
+         $consulta->execute();
         $resultados = $consulta->fetch(PDO::FETCH_ASSOC);
-        $correo_cliente_enviar = $resultados['email'];
-        $header = "From: sigproIngenieria@gmail.com". "\r\n";
-        $header.="Reply-To: sigproIngenieria@gmail.com"."\r\n";
-        $header.="X-Mailer: PHP/".phpversion();
-        $mail = @mail($correo_cliente_enviar,$asunto,$mensaje_correo,$header);
+        $correo_cliente = $resultados['email'];
+        $header = "From: sigproIngenieria@example.com"."\r\n";
+        $header .= "Reply-To: sigproIngenieria@example.com"."\r\n";
+        $header.= "X-Mailer: PHP/".phpversion();
+        $mail =@mail($correo_cliente,$asunto,$mensaje,$header);
         if($mail){
-            echo "enviadooooooooooo";
+            echo "Enviadoooo";
         }
     }
 
